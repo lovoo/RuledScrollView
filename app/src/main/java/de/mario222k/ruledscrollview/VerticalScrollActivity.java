f97bfdc1ca637bf7cac1ac8f9102fb6f26fdb825
Steffen Funke <info@steffen-funke.de>
2014-10-31 16:26:59 +0100

    tested requestDisallowInterceptTouchEvent impl, does not work well when scroll on inner buttons

--- app/src/main/java/de/mario222k/ruledscrollview/VerticalScrollActivity.java
+++ app/src/main/java/de/mario222k/ruledscrollview/VerticalScrollActivity.java
@@ -2,38 +2,70 @@ package de.mario222k.ruledscrollview;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.ScrollView;
+import android.widget.Toast;
 
 
 public class VerticalScrollActivity extends Activity {
 
-    @Override
-    protected void onCreate ( Bundle savedInstanceState ) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_example);
-    }
+	private View.OnTouchListener mTouchListener;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_example);
+		init();
+	}
+
+	private void init() {
+		final ScrollView outer = (ScrollView) findViewById(R.id.outerScrollView);
+		final ScrollView inner1 = (ScrollView) findViewById(R.id.innerScrollView1);
+		final ScrollView inner2 = (ScrollView) findViewById(R.id.innerScrollView2);
+
+		mTouchListener = new View.OnTouchListener() {
+			public float oldy;
+			private int dir;
+
+			@Override
+			public boolean onTouch(View v, MotionEvent event) {
+				switch (event.getAction()) {
+
+					case MotionEvent.ACTION_DOWN:
+						oldy = event.getY(); // set start y pos
+						Log.d("XXX", "inner down");
+						break;
+
+					case MotionEvent.ACTION_MOVE:
+						Log.d("XXX", "inner move");
+						// get direction of touch
+						dir = (int) (oldy - event.getY());
+
+						// if we can scroll in this direction, please parent to not intercept event
+						if (v.canScrollVertically(dir)) {
+							outer.requestDisallowInterceptTouchEvent(true);
+						} else {
+							outer.requestDisallowInterceptTouchEvent(false);
+						}
+						break;
+
+					case MotionEvent.ACTION_UP:
+						Log.d("XXX", "inner up");
+						break;
+				}
+				return false;
+			}
+		};
+
+		inner1.setOnTouchListener(mTouchListener);
+		inner2.setOnTouchListener(mTouchListener);
+	}
 
 
-    @Override
-    public boolean onCreateOptionsMenu ( Menu menu ) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_example, menu);
-        return true;
-    }
+	public void onButtonClick(View button) {
+		Toast.makeText(this, "Clicked!", Toast.LENGTH_SHORT).show();
+	}
 
-    @Override
-    public boolean onOptionsItemSelected ( MenuItem item ) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
